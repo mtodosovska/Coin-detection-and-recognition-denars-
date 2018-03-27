7ee9136eb6ab0974c2dddf0b67fc5a102a53bcb9
Marija <mtodosovska@gmail.com>
2018-03-28 00:32:42 +0200

    Added the coin diameters, changed the input method.

--- coin_detection.py
+++ coin_detection.py
@@ -9,11 +9,11 @@ import glob
 import cv2
 
 # construct argument parser and parse arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="path to image")
-args = vars(ap.parse_args())
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-i", "--image", required=True, help="path to image")
+# args = vars(ap.parse_args())
 
-image = cv2.imread(args["image"])
+image = cv2.imread("Images/input/i.1.jpg")
 
 # resize image while retaining aspect ratio
 d = 1024 / image.shape[1]
@@ -184,19 +184,13 @@ biggest = max(diameter)
 i = diameter.index(biggest)
 
 # scale everything according to maximum diameter
-# todo: change diameters
-if materials[i] == "Euro2":
-    diameter = [x / biggest * 25.75 for x in diameter]
-    scaledTo = "Scaled to 2 Euro"
-elif materials[i] == "Brass":
-    diameter = [x / biggest * 24.25 for x in diameter]
-    scaledTo = "Scaled to 50 Cent"
-elif materials[i] == "Euro1":
-    diameter = [x / biggest * 23.25 for x in diameter]
-    scaledTo = "Scaled to 1 Euro"
+if materials[i] == "Silver":
+    diameter = [x / biggest * 26.4 for x in diameter]
+    scaledTo = "Scaled to 50."
 elif materials[i] == "Copper":
-    diameter = [x / biggest * 21.25 for x in diameter]
-    scaledTo = "Scaled to 5 Cent"
+    diameter = [x / biggest * 27.35 for x in diameter]
+    scaledTo = "Scaled to 5."
+
 else:
     scaledTo = "unable to scale.."
 
@@ -208,31 +202,21 @@ while i < len(diameter):
     (x, y) = coordinates[i]
     t = "Unknown"
 
-    #todo: change diameters
     # compare to known diameters with some margin for error
-    if math.isclose(d, 25.75, abs_tol=1.25) and m == "Euro2":
-        t = "2 Euro"
-        total += 200
-    elif math.isclose(d, 23.25, abs_tol=2.5) and m == "Euro1":
-        t = "1 Euro"
-        total += 100
-    elif math.isclose(d, 19.75, abs_tol=1.25) and m == "Brass":
-        t = "10 Cent"
+    if math.isclose(d, 24.6, abs_tol=1.25) and m == "Silver":
+        t = "10"
         total += 10
-    elif math.isclose(d, 22.25, abs_tol=1.0) and m == "Brass":
-        t = "20 Cent"
-        total += 20
-    elif math.isclose(d, 24.25, abs_tol=2.5) and m == "Brass":
-        t = "50 Cent"
+    elif math.isclose(d, 26.4, abs_tol=1.3) and m == "Silver":
+        t = "50"
         total += 50
-    elif math.isclose(d, 16.25, abs_tol=1.25) and m == "Copper":
-        t = "1 Cent"
+    elif math.isclose(d, 23.65, abs_tol=1.25) and m == "Copper":
+        t = "1"
         total += 1
-    elif math.isclose(d, 18.75, abs_tol=1.25) and m == "Copper":
-        t = "2 Cent"
+    elif math.isclose(d, 25.3, abs_tol=1.25) and m == "Copper":
+        t = "2"
         total += 2
-    elif math.isclose(d, 21.25, abs_tol=2.5) and m == "Copper":
-        t = "5 Cent"
+    elif math.isclose(d, 27.35, abs_tol=1.35) and m == "Copper":
+        t = "5"
         total += 5
 
     # write result on output image
@@ -246,6 +230,7 @@ d = 768 / output.shape[1]
 dim = (768, int(output.shape[0] * d))
 image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
 output = cv2.resize(output, dim, interpolation=cv2.INTER_AREA)
+
 
 # write summary on output image
 cv2.putText(output, scaledTo,
