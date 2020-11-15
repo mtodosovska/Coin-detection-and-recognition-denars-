a05f310eff47bf16ff688ffd6984df255d40601e
Marija Todosovska <Marija.Todosovska@netcetera.com>
2020-11-16 00:21:16 +0100

    Refactored.

--- coin-detection.py
+++ coin-detection.py
@@ -4,248 +4,127 @@ from sklearn.model_selection import train_test_split
 
 import math
 import numpy as np
-import argparse
 import glob
 import cv2
 
-# construct argument parser and parse arguments
-ap = argparse.ArgumentParser()
-#ap.add_argument("-i", "--image", required=True, help="path to image")
-args = vars(ap.parse_args())
-
-#image = cv2.imread(args["image"])
 image = cv2.imread('Images\\input\\i.5.jpg')
-# resize image while retaining aspect ratio
 d = 1024 / image.shape[1]
 dim = (1024, int(image.shape[0] * d))
 image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
 
-# create a copy of the image to display results
 output = image.copy()
 
-# convert image to grayscale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# improve contrast accounting for differences in lighting conditions:
-# create a CLAHE object to apply contrast limiting adaptive histogram equalization
 clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
 gray = clahe.apply(gray)
 
 
-def calcHistogram(img):
+def get_histogram(image):
     # create mask
-    m = np.zeros(img.shape[:2], dtype="uint8")
-    (w, h) = (int(img.shape[1] / 2), int(img.shape[0] / 2))
-    cv2.circle(m, (w, h), 60, 255, -1)
+    mask = np.zeros(image.shape[:2], dtype="uint8")
+    (width, height) = (int(image.shape[1] / 2), int(image.shape[0] / 2))
+    cv2.circle(mask, (width, height), radius=60, color=(255, 255, 255))
 
     # calcHist expects a list of images, color channels, mask, bins, ranges
-    h = cv2.calcHist([img], [0, 1, 2], m, [8, 8, 8], [0, 256, 0, 256, 0, 256])
+    height = cv2.calcHist([image], [0, 1, 2], mask, [16, 16, 16], [0, 256, 0, 256, 0, 256])
 
-    # return normalized "flattened" histogram
-    return cv2.normalize(h, h).flatten()
+    return cv2.normalize(height, height).flatten()
 
 
-def calcHistFromFile(file):
+def get_hist_from_file(file):
     img = cv2.imread(file)
-    return calcHistogram(img)
+    return get_histogram(img)
 
 
-# define Enum class
-class Enum(tuple): __getattr__ = tuple.index
-
-
-# Enumerate material types for use in classifier
-# Material = Enum(('Copper', 'Brass', 'Euro1', 'Euro2'))
-Material = Enum(('Copper', 'Silver'))
-
-# locate sample image files
-# sample_images_copper = glob.glob("sample_images/copper/*")
-# sample_images_brass = glob.glob("sample_images/brass/*")
-# sample_images_euro1 = glob.glob("sample_images/euro1/*")
-# sample_images_euro2 = glob.glob("sample_images/euro2/*")
-
-sample_images_copper = glob.glob("images/copper/*")
-sample_images_silver = glob.glob("images/silver/*")
-
-# define training data and labels
-X = []
-y = []
-
-# compute and store training data and labels
-# for i in sample_images_copper:
-#     X.append(calcHistFromFile(i))
-#     y.append(Material.Copper)
-# for i in sample_images_brass:
-#     X.append(calcHistFromFile(i))
-#     y.append(Material.Brass)
-# for i in sample_images_euro1:
-#     X.append(calcHistFromFile(i))
-#     y.append(Material.Euro1)
-# for i in sample_images_euro2:
-#     X.append(calcHistFromFile(i))
-#     y.append(Material.Euro2)
-
-for i in sample_images_copper:
-    X.append(calcHistFromFile(i))
-    y.append(Material.Copper)
-for i in sample_images_silver:
-    X.append(calcHistFromFile(i))
-    y.append(Material.Silver)
-
-# instantiate classifier
-# Multi-layer Perceptron
-# score: 0.974137931034
-clf = MLPClassifier(solver="lbfgs")
-
-# split samples into training and test data
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=.2)
-
-# train and score classifier
-clf.fit(X_train, y_train)
-score = int(clf.score(X_test, y_test) * 100)
-print("Classifier mean accuracy: ", score)
-
-# blur the image using Gaussian blurring, where pixels closer to the center
-# contribute more "weight" to the average, first argument is the source image,
-# second argument is kernel size, third one is sigma (0 for autodetect)
-# we use a 7x7 kernel and let OpenCV detect sigma
-blurred = cv2.GaussianBlur(gray, (7, 7), 0)
-
-# circles: A vector that stores x, y, r for each detected circle.
-# src_gray: Input image (grayscale)
-# CV_HOUGH_GRADIENT: Defines the detection method.
-# dp = 2.2: The inverse ratio of resolution
-# min_dist = 100: Minimum distance between detected centers
-# param_1 = 200: Upper threshold for the internal Canny edge detector
-# param_2 = 100*: Threshold for center detection.
-# min_radius = 50: Minimum radius to be detected.
-# max_radius = 120: Maximum radius to be detected.
-circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, dp=2.2, minDist=100,
-                           param1=200, param2=100, minRadius=50, maxRadius=120)
-
-
-def predictMaterial(roi):
-    # calculate feature vector for region of interest
-    hist = calcHistogram(roi)
-
-    # predict material type
+def get_material(clf, roi):
+    hist = get_histogram(roi)
     s = clf.predict([hist])
-
-    # return predicted material type
     return Material[int(s)]
 
 
-# todo: refactor
+class Enum(tuple): __getattr__ = tuple.index
+
+
+Material = Enum(('Copper', 'Silver'))
+
+images_copper = glob.glob("images/copper/*")
+images_silver = glob.glob("images/silver/*")
+
+X = []
+y = []
+
+
+for i in images_copper:
+    X.append(get_hist_from_file(i))
+    y.append(Material.Copper)
+for i in images_silver:
+    X.append(get_hist_from_file(i))
+    y.append(Material.Silver)
+
+clf = MLPClassifier(solver="lbfgs")
+
+# split samples into training and test data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
+
+clf.fit(X_train, y_train)
+score = clf.score(X_train, y_train) * 100
+print("Classifier train mean accuracy: ", score)
+score = clf.score(X_test, y_test) * 100
+print("Classifier test mean accuracy: ", score)
+
+blurred = cv2.GaussianBlur(gray, (7, 7), 0)
+circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, dp=2.2, minDist=100, param1=200, param2=100, minRadius=50, maxRadius=120)
+
+
 diameter = []
 materials = []
 coordinates = []
 
 count = 0
 if circles is not None:
-    # append radius to list of diameters (we don't bother to multiply by 2)
     for (x, y, r) in circles[0, :]:
-        diameter.append(r)
+        diameter.append(2*r)
 
-    # convert coordinates and radii to integers
     circles = np.round(circles[0, :]).astype("int")
 
-    # loop over coordinates and radii of the circles
     for (x, y, d) in circles:
         count += 1
-
-        # add coordinates to list
         coordinates.append((x, y))
-
-        # extract region of interest
         roi = image[y - d:y + d, x - d:x + d]
-
-        # try recognition of material type and add result to list
-        material = predictMaterial(roi)
+        material = get_material(clf, roi)
         materials.append(material)
 
-        # write masked coin to file
-        if False:
-            m = np.zeros(roi.shape[:2], dtype="uint8")
-            w = int(roi.shape[1] / 2)
-            h = int(roi.shape[0] / 2)
-            cv2.circle(m, (w, h), d, (255), -1)
-            maskedCoin = cv2.bitwise_and(roi, roi, mask=m)
-            cv2.imwrite("extracted/01coin{}.png".format(count), maskedCoin)
-
-        # draw contour and results in the output image
-        cv2.circle(output, (x, y), d, (0, 255, 0), 2)
-        cv2.putText(output, material,
-                    (x - 40, y), cv2.FONT_HERSHEY_PLAIN,
-                    1.5, (0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
-
-# get biggest diameter
-biggest = max(diameter)
-i = diameter.index(biggest)
-
-# scale everything according to maximum diameter
-# todo: change diameters
-if materials[i] == "Silver":
-    diameter = [x / biggest * 19.45 for x in diameter]
-    scaledTo = "Scaled to 50 denars"
-elif materials[i] == "Copper":
-    diameter = [x / biggest * 21.95 for x in diameter]
-    scaledTo = "Scaled to 5 denars"
-else:
-    scaledTo = "unable to scale.."
+        cv2.circle(output, (x, y), d, (187, 255, 0), 3)
+        cv2.putText(output, material, (x - 40, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (187, 255, 0), thickness=2, lineType=cv2.LINE_4)
 
 i = 0
 total = 0
+
+known_diameters = {
+                    '10 den': [19.25, 0.75, 'Silver', 10],
+                    '50 den': [21.45, 1.45, 'Silver', 50],
+                    '1 den': [17.95, 1.25, 'Copper', 1],
+                    '2 den': [19.25, 1.25, 'Copper', 2],
+                    '5 den': [21.95, 2.5, 'Copper', 5]
+                   }
 while i < len(diameter):
     d = diameter[i]
     m = materials[i]
     (x, y) = coordinates[i]
     t = "Unknown"
 
-
-    #todo: change diameters
-    # compare to known diameters with some margin for error
-    if math.isclose(d, 19.25, abs_tol=0.75) and m == "Silver":
-        t = "10 den"
-        total += 10
-    elif math.isclose(d, 21.45, abs_tol=1.45) and m == "Silver":
-        t = "50 den"
-        total += 50
-    elif math.isclose(d, 17.95, abs_tol=1.25) and m == "Copper":
-        t = "1 den"
-        total += 1
-    elif math.isclose(d, 19.25, abs_tol=1.25) and m == "Copper":
-        t = "2 den"
-        total += 2
-
-    elif math.isclose(d, 21.95, abs_tol=2.5) and m == "Copper":
-        t = "5 den"
-        total += 5
+    for key in known_diameters:
+        if math.isclose(d, known_diameters[key][0], abs_tol=known_diameters[key][1]) and m == known_diameters[key][2]:
+            t = key
+            total += known_diameters[key][3]
 
     # write result on output image
-    cv2.putText(output, t,
-                (x - 40, y + 22), cv2.FONT_HERSHEY_PLAIN,
-                1.5, (255, 255, 255), thickness=2, lineType=cv2.LINE_AA)
+    cv2.putText(output, t, (x - 40, y + 22), cv2.FONT_HERSHEY_PLAIN, 1.5, (3, 186, 252), thickness=2, lineType=cv2.LINE_4)
     i += 1
 
-# resize output image while retaining aspect ratio
-d = 768 / output.shape[1]
-dim = (768, int(output.shape[0] * d))
-image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
-output = cv2.resize(output, dim, interpolation=cv2.INTER_AREA)
+print('======= Summary: =======')
+print(f'Coins detected: {count}, MKD {total:2}')
 
-# write summary on output image
-cv2.putText(output, scaledTo,
-            (5, output.shape[0] - 40), cv2.FONT_HERSHEY_PLAIN,
-            1.0, (0, 0, 255), lineType=cv2.LINE_AA)
-cv2.putText(output, "Coins detected: {}, MKD {:2}".format(count, total),
-            (5, output.shape[0] - 24), cv2.FONT_HERSHEY_PLAIN,
-            1.0, (0, 0, 255), lineType=cv2.LINE_AA)
-cv2.putText(output, "Classifier mean accuracy: {}%".format(score),
-            (5, output.shape[0] - 8), cv2.FONT_HERSHEY_PLAIN,
-            1.0, (0, 0, 255), lineType=cv2.LINE_AA)
-
-# show output and wait for key to terminate program
-#cv2.imshow("Output", np.hstack([image, output]))
-cv2.imshow('out',output)
+cv2.imshow('out', output)
 cv2.waitKey(0)
